4bf78ddbb0d056ea8afa1879d6be8e7561d8d07d
Aniket <aniketsupekar2004@gmail.com>
2026-02-11 15:34:04 +0530

    admin added

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-from flask import Flask , session
+from flask import Flask, session
 
 def create_app():
     app = Flask(__name__)
-
     app.secret_key = 'super_secret_key'
 
     @app.before_request
     def set_default_user():
-        email = "Pilsecurity.CS01@pipelineinfra.com"
-        location = email.split('@')[0].split('.')[-1]
 
-        if 'user' not in session:
-            session['user'] = {}
+        email = "Pilsecurity.CS02@pipelineinfra.com"
 
-        session['user'].update({
+        username_part = email.split('@')[0]
+        location = username_part.split('.')[-1].upper()
+        name_part = username_part.replace(f".{location}", "")
+        name = name_part.replace('.', ' ').title()
+
+        if email.lower() == "admin@pipelineinfra.com":
+            role = "admin"
+        else:
+            role = "user"
+
+        session['user'] = {
             "email": email,
-            "name": "PIL Security",
-            "location": location
-        })
-
-
-
+            "name": name,
+            "location": location,
+            "role": "admin" or "user"
+        }
 
     from route import routes_bp
     app.register_blueprint(routes_bp)
 
-
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(host='0.0.0.0', port=5001, debug=True)
